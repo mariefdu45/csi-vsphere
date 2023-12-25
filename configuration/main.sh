0c5cc066a150fe7e9b1a2f84103784c2822e5f73
mariefdu45 <mariedu45.f@gmail.com>
2023-12-25 08:42:21 +0100

    csi removing add on

--- configuration/main.sh
+++ configuration/main.sh
@@ -1,10 +1,21 @@
 # !/bin/bash
 # CSI Installation – Main Program
-# Version : December 24, 2023
+# Version : December 25, 2023
 # Author : mariefdu45@gmail.com
 #
 source govc.env
 source variables.env
-source ./configuration/csi_user_configuration
-source ./configuration/cpi_configuration
-source ./configuration/csi_configuration
+
+if [[ $1 == "install" ]]
+then
+    source ./configuration/csi_user_configuration
+    source ./configuration/cpi_configuration
+    source ./configuration/csi_configuration
+elif [[ $1 == "uninstall" ]] 
+then
+    source ./configuration/csi_removing
+    source ./configuration/cpi_removing
+	source ./configuration/csi_user_removing
+else
+    echo "Error. Usage: main.sh install|uninstall"
+fi
